7d697e94a6f0625b5dc5627c87e0fd9a171caed4
Jeff <jeffersonorozco24@gmail.com>
2026-08-06 00:11:41 -0500

    REFACTOR: change application service to relate with auth user

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,9 +1,44 @@
-from app.core.database import SessionLocal
+from fastapi import Depends, HTTPException, status
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+
+from app.core.database import get_db
+from app.core.security import SECRET_KEY, ALGORITHM
+from app.models.user import User
+from app.services.user_service import get_user_by_email
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
-def get_db():
-    db = SessionLocal()
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid authentication credentials",
+    )
+
     try:
-        yield db
-    finally:
-        db.close()
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        email = payload.get("sub")
+
+        if email is None:
+            raise credentials_exception
+
+    except JWTError:
+        raise credentials_exception
+
+    user = get_user_by_email(db, email)
+
+    if user is None:
+        raise credentials_exception
+
+    return user
